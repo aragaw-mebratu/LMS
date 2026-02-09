3b8b284243c9398b4e8bb270a0426c7429493fd8
Aragaw Mebratu <aragawmebratu65@gmail.com>
2026-02-09 21:18:20 +0300

    Custome user model and profile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / 'templates'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'userauths.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True,max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100,unique=True)
+    otp = models.CharField(unique=True, max_length=100)
+
+    USERNAME_FIELD = 'email' # Users authenticate with email, not username
+    REQUIRED_FIELDS = ['username'] # Username is required when creating users via CLI
+
+    def __str__(self):
+        return self.email
+    
+    def save(self,*args,**kwargs):
+        email_username, full_name = self.email.split("@")
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = email_username
+        if self.username == "" or self.username == None:
+            self.username = email_username
+        super(User,self).save(*args, **kwargs)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="user_folder",default="default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    about = models.TextField(null=True,blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+    def save(self,*args, **kwargs):
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = self.user.username
+        super(Profile, self).save(*args, **kwargs)
