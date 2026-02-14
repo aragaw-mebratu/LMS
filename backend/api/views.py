df427fc4d8d262567c25545cc73d8e1e11c9be9d
Aragaw Mebratu <aragawmebratu65@gmail.com>
2026-02-14 21:06:06 +0300

    password reset

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,10 +2,11 @@ import random
 from django.shortcuts import render
 from api import serializer as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics
+from rest_framework import generics,status
 from userauths.models import User,Profile
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 # Create your views here.
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -40,3 +41,22 @@ class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
             link = f"http://localhost:5173/create-new-password/?opt={user.otp}&uuidb64={uuidb64}&=refresh_token={refresh_token}"
             print("link =====", link)
         return user
+class PasswordChangeAPIView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        otp = request.data['otp']
+        uuidb64 = request.data['uuidb64']
+        password = request.data['password']
+
+        user = User.objects.get(id=uuidb64,otp=otp)
+        if user:
+            user.set_password(password)
+            user.otp = ""
+            user.save()
+            return Response({"message": "Password Changed Successfully"},status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message":"User does not exist"},status=status.HTTP_404_NOT_FOUND)
+
+    
