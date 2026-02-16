442c6db1439cdfdc28b65f3b08fb39403d08dbde
Aragaw Mebratu <aragawmebratu65@gmail.com>
2026-02-16 21:09:10 +0300

    email configuration

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 import random
 from django.shortcuts import render
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from api import serializer as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics,status
@@ -7,6 +9,7 @@ from userauths.models import User,Profile
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
+from django.conf import settings
 # Create your views here.
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -39,6 +42,22 @@ class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
             user.save()
 
             link = f"http://localhost:5173/create-new-password/?opt={user.otp}&uuidb64={uuidb64}&=refresh_token={refresh_token}"
+            context = {
+                "link": link,
+                "username": user.username
+            }
+            subject = "Password Reset Email"
+            text_body = render_to_string("email/password_reset.txt",context)
+            html_body = render_to_string("email/password_reset.html",context)
+
+            msg = EmailMultiAlternatives(
+                subject=subject,
+                from_email=settings.FROM_EMAIL,
+                to=[user.email],
+                body=text_body
+            )
+            msg.attach_alternative(html_body, "text/html")
+            msg.send()
             print("link =====", link)
         return user
 class PasswordChangeAPIView(generics.CreateAPIView):
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,9 @@ INSTALLED_APPS = [
     #Third Party Apps
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'corsheaders'
+    'corsheaders',
+    'anymail',
+
 ]
 
 MIDDLEWARE = [
@@ -138,8 +140,16 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'userauths.User'
 
-MAILGUN_SECRET_KEY=env("MAILGUN_SECRET_KEY")
+MAILGUN_API_KEY= env("MAILGUN_API_KEY")
+MAILERSEND_API_TOKEN= env("MAILERSEND_API_TOKEN")
+MAILGUN_SENDER_DOMAIN = env("MAILGUN_SENDER_DOMAIN")
 
+ANYMAIL = {
+    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
+    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_SENDER_DOMAIN"),
+}
+FROM_EMAIL = env("FROM_EMAIL")
+EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
